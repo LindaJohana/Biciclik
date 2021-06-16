9715f7704b3b82827c7c08364657cfbd9dfb2e89
Linda <linda.patarroyo@unillanos.edu.co>
2021-06-16 12:03:05 -0500

    vista Bici información al terminar un viaje de la app Android.

---
+++ app/src/main/java/com/example/biciclik/Trip/TripActivity.java
@@ -0,0 +1,18 @@
+package com.example.biciclik.Trip;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.biciclik.R;
+
+public class TripActivity extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container, @NonNull Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.trip, container, false);
+        return view;
+    }
+}
