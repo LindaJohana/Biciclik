4a32596abd559775c156dc550e069072b9a1cdf0
Cristian Dulcey <info@loducode.com>
2021-06-24 20:46:43 -0500

    unlock and connect

--- app/src/main/java/com/example/biciclik/TakeBici/TakeBiciActivity.java
+++ app/src/main/java/com/example/biciclik/TakeBici/TakeBiciActivity.java
@@ -14,19 +14,35 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
+
 import androidx.fragment.app.FragmentTransaction;
 
-import com.blikoon.qrcodescanner.QrCodeActivity;
+
 import com.example.biciclik.BaseContext.BaseContext;
 import com.example.biciclik.R;
-import com.google.zxing.qrcode.encoder.QRCode;
 import com.omni.support.ble.BleModuleHelper;
+import com.omni.support.ble.core.ICommand;
+import com.omni.support.ble.core.IResp;
 import com.omni.support.ble.core.ISessionCall;
+import com.omni.support.ble.core.NotifyCallback;
 import com.omni.support.ble.core.SessionCallback;
+import com.omni.support.ble.protocol.bike.model.BLLockResult;
 import com.omni.support.ble.rover.CommandManager;
+import com.omni.support.ble.session.ISessionListener;
+import com.omni.support.ble.session.SimpleSessionListener;
 import com.omni.support.ble.session.sub.Bike3In1Session;
 
+import org.jetbrains.annotations.NotNull;
+
+import kotlin.Metadata;
+import kotlin.jvm.internal.Intrinsics;
+
+@Metadata(
+        mv = {1, 5, 1},
+        k = 1,
+        d1 = {"\u0000\u001a\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0002\u0018\u00002\u00020\u0001B\u0005¢\u0006\u0002\u0010\u0002J\u0006\u0010\u0005\u001a\u00020\u0006J\u0006\u0010\u0007\u001a\u00020\u0006R\u000e\u0010\u0003\u001a\u00020\u0004X\u0082.¢\u0006\u0002\n\u0000¨\u0006\b"},
+        d2 = {"Lcom/example/biciclik/utils/BikeTestActivity;", "", "()V", "session", "Lcom/omni/support/ble/session/sub/Bike3In1Session;", "init", "", "unlock", "Biciclik.app"}
+)
 
 public class TakeBiciActivity extends Fragment implements TakeBiciInterfaces.activities{
 
@@ -39,41 +55,112 @@ public class TakeBiciActivity extends Fragment implements TakeBiciInterfaces.act
     private Bike3In1Session session;
 
 
+
+
     public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container, @NonNull Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.take_bici, container, false);
         initObjects(view);
         int requestCode = 200;
-        BleModuleHelper.INSTANCE.init(this.getActivity().getApplication());//java
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             requestPermissions(permissions, requestCode);
         }
         fragmentTrip1=new TakeBici2Fragment();
         getChildFragmentManager().beginTransaction().commit();
-        session = new Bike3In1Session.Builder()
-                .address("88:BF:E4:BC:6C:D8")//Replace your mac address
-                .deviceKey("yOTmK50z")
-                .deviceType("A1")
-                .updateKey("Vgz7")
-                .build();
+        BleModuleHelper.INSTANCE.init(this.getActivity().getApplication());//java
+
+
+
+
+        this.session = (Bike3In1Session)(new Bike3In1Session.Builder()).address("E3:1D:F1:0F:33:5B").deviceKey("yOTmK50z").deviceType("A1").updateKey("Vgz7").build();
+        Bike3In1Session var10000 = this.session;
+
+        if (var10000 == null) {
+            Intrinsics.throwUninitializedPropertyAccessException("session");
+        }
+
+        var10000.setListener((ISessionListener)(new SimpleSessionListener() {
+            public void onConnecting() {
+            }
+
+            public void onConnected() {
+            }
+
+            public void onDisconnected() {
+            }
+
+            public void onDeviceNoSupport() {
+            }
+
+            public void onReady() {
+                TakeBiciActivity.access$getSession$p(TakeBiciActivity.this).call((ICommand) CommandManager.INSTANCE.getBlCommand().lock()).subscribe((NotifyCallback)(new NotifyCallback() {
+                    public void onSuccess(@NotNull ISessionCall call, @NotNull IResp data) {
+                        Intrinsics.checkNotNullParameter(call, "call");
+                        Intrinsics.checkNotNullParameter(data, "data");
+                        BLLockResult result = (BLLockResult)data.getResult();
+                        if (result != null) {
+                            Log.d("=====", result.toString());
+                        }
+
+                        TakeBiciActivity.access$getSession$p(TakeBiciActivity.this).call((ICommand)CommandManager.INSTANCE.getBlCommand().lockReply()).execute();
+                    }
+                }));
+            }
+        }));
+
         ButtonQR.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                leerQR();
+//                leerQR();
+                unlock();
+
 
             }
         });
 
 
-
-
-
-
-
         return view;
+    }
+
+    public final void unlock() {
+        Bike3In1Session var10000 = this.session;
+        if (var10000 == null) {
+            Intrinsics.throwUninitializedPropertyAccessException("session");
+        }
+
+        var10000.call((ICommand)CommandManager.INSTANCE.getBlCommand().unlock(0, System.currentTimeMillis() / (long)1000, 0)).timeout(3000L).enqueue((SessionCallback)(new SessionCallback() {
+            public void onSuccess(@NotNull ISessionCall call, @NotNull IResp data) {
+                Boolean var10000 = (Boolean)data.getResult();
+                boolean isSuccess = var10000 != null ? var10000 : false;
+                Toast.makeText(BaseContext.getContext(), (CharSequence)(isSuccess ? "Successfully unlocked" : "Failed to unlock"), Toast.LENGTH_LONG).show();
+                TakeBiciActivity.access$getSession$p(TakeBiciActivity.this).call((ICommand)CommandManager.INSTANCE.getBlCommand().unlockReply()).execute();
+            }
+
+            public void onFailure(@NotNull ISessionCall call, @NotNull Throwable e) {
+                Toast.makeText(BaseContext.getContext(), "fallo", Toast.LENGTH_LONG).show();
+                Intrinsics.checkNotNullParameter(call, "call");
+                Intrinsics.checkNotNullParameter(e, "e");
+            }
+        }));
+    }
+
+    // $FF: synthetic method
+    public static final Bike3In1Session access$getSession$p(TakeBiciActivity $this) {
+        Bike3In1Session var10000 = $this.session;
+        if (var10000 == null) {
+            Intrinsics.throwUninitializedPropertyAccessException("session");
+        }
+
+        return var10000;
+    }
+
+    public static final void access$setSession$p(TakeBiciActivity $this, Bike3In1Session var1) {
+        $this.session = var1;
     }
 
     public void initObjects(View view){
         ButtonQR=view.findViewById(R.id.buttonQR);
+
 
     }
 
--- app/src/main/java/com/example/biciclik/utils/BikeTestActivity.kt
+++ app/src/main/java/com/example/biciclik/utils/BikeTestActivity.kt
@@ -1,29 +1,43 @@
 package com.example.biciclik.utils
 
+import android.os.Bundle
 import android.util.Log
-import com.omni.support.ble.core.IResp
-import com.omni.support.ble.core.ISessionCall
-import com.omni.support.ble.core.NotifyCallback
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.biciclik.R
+import com.omni.support.ble.BleModuleHelper
+import com.omni.support.ble.core.*
+import com.omni.support.ble.protocol.bike.model.BLInfoResult
 import com.omni.support.ble.protocol.bike.model.BLLockResult
+import com.omni.support.ble.protocol.bike.model.BLShutdownResult
 import com.omni.support.ble.rover.CommandManager
 import com.omni.support.ble.session.SimpleSessionListener
 import com.omni.support.ble.session.sub.Bike3In1Session
 
-class BikeTestActivity : BaseActivity {
-
-
+class BikeTestActivity : AppCompatActivity() {
     private lateinit var session: Bike3In1Session
+    private lateinit var btn_connect: Button
+    private lateinit var btn_disconnect: Button
+    private lateinit var btn_unlock: Button
+    private lateinit var btn_info: Button
+    private lateinit var btn_shutdown: Button
+    private lateinit var btn_get_log: Button
 
-
-    override fun init() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bike)
+        BleModuleHelper.init(this.application)
         session = Bike3In1Session.Builder()
-                .address("C6:01:BD:63:01:70")//Replace your mac address
-                .deviceKey("yOTmK50z")
-                .deviceType("A1")
-                .updateKey("Vgz7")
-                .build()
+            .address("E3:1D:F1:0F:33:5B")//Replace your mac address
+            .deviceKey("yOTmK50z")
+            .deviceType("A1")
+            .updateKey("Vgz7")
+            .build()
+
         session.setListener(object : SimpleSessionListener() {
             override fun onConnecting() {
+
             }
 
             override fun onConnected() {
@@ -36,110 +50,146 @@ class BikeTestActivity : BaseActivity {
             }
 
             override fun onReady() {
-// Lock monitor
+                // Lock monitor
                 session.call(CommandManager.blCommand.lock())
-                        .subscribe(object : NotifyCallback<BLLockResult> {
-                            override fun onSuccess(call: ISessionCall<BLLockResult>,
-                                                   data: IResp<BLLockResult>) {
-                                val result = data.getResult()
-                                if (result != null) {
-                                    Log.d("=====", result.toString())
-                                }
-// Close lock reply
-                                session.call(CommandManager.blCommand.lockReply()).execute()
+                    .subscribe(object : NotifyCallback<BLLockResult> {
+                        override fun onSuccess(
+                            call: ISessionCall<BLLockResult>,
+                            data: IResp<BLLockResult>
+                        ) {
+                            val result = data.getResult()
+                            if (result != null) {
+                                Log.d("=====", result.toString())
                             }
-                        })
+                            // Close lock reply
+                            session.call(CommandManager.blCommand.lockReply()).execute()
+                        }
+                    })
             }
+
         })
+        btn_connect = findViewById<Button>(R.id.btn_connect)
+        btn_disconnect = findViewById<Button>(R.id.btn_disconnect)
+        btn_unlock = findViewById<Button>(R.id.btn_unlock)
+        btn_info = findViewById<Button>(R.id.btn_info)
+        btn_shutdown = findViewById<Button>(R.id.btn_shutdown)
+        btn_get_log = findViewById<Button>(R.id.btn_get_log)
+        initListener()
     }
-                override fun initListener() {
-// connection
-            btn_connect.setOnClickListener {
-                session.connect()
-            }
-// Disconnect
-            btn_disconnect.setOnClickListener {
-                session.disConnect()
-            }
-//unlock
-            btn_unlock.setOnClickListener {
-                session.call(CommandManager.blCommand.unlock(0,
-                        System.currentTimeMillis() / 1000, 0))
-                        .timeout(3000)
-                        .enqueue(object : SessionCallback<Boolean> {
-                            override fun onSuccess(call: ISessionCall<Boolean>, data:
-                            IResp<Boolean>) {
-                                val isSuccess = data.getResult() ?: false
-                                Toast.makeText(this@BikeLockTestActivity, if (isSuccess)
-                                    "Successfully unlocked" else "Failed to unlock", Toast.LENGTH_SHORT)
-                                        .show()
-// Unlock reply
-                                session.call(CommandManager.blCommand.unlockReply()).execute()
-                            }
 
-                            override fun onFailure(call: ISessionCall<Boolean>, e:
-                            Throwable) {
-                            }
-                        })
-            }
-//Get lock information
-            btn_info.setOnClickListener {
-                session.call(CommandManager.blCommand.getLockInfo())
-                        .enqueue(object : SessionCallback<BLInfoResult> {
-                            override fun onSuccess(call: ISessionCall<BLInfoResult>, data:
-                            IResp<BLInfoResult>) {
-                                val result = data.getResult()
-                                if (result != null) {
-                                    Log.d("=====", result.toString())
-                                }
-                            }
+    fun initListener() {
 
-                            override fun onFailure(call: ISessionCall<BLInfoResult>, e:
-                            Throwable) {
-                            }
-                        })
-            }
-//Shutdown
-            btn_shutdown.setOnClickListener {
-                session.call(CommandManager.blCommand.shutdown())
-                        .enqueue(object : SessionCallback<BLShutdownResult> {
-                            override fun onSuccess(call: ISessionCall<BLShutdownResult>,
-                                                   data: IResp<BLShutdownResult>) {
-                                val result = data.getResult()
-                                if (result != null) {
-                                    Log.d("=====", result.toString())
-                                }
-                            }
-
-                            override fun onFailure(call: ISessionCall<BLShutdownResult>, e:
-                            Throwable) {
-                            }
-                        })
-            }
-//Get log
-            btn_get_log.setOnClickListener {
-                session.call(CommandManager.blCommand.getLog())
-                        .asyncTimeout(10000)
-                        .asyncCall(object : AsyncCallback<String> {
-                            override fun onTimeout() {
-                            }
-
-                            override fun onStarted(success: Boolean) {
-                            }
-
-                            override fun onReceiving(call: ISessionCall<String>, data:
-                            IResp<String>) {
-                                Log.d("=====", "${data.getResult()}")
-                            }
-
-                            override fun onReceived() {
-                            }
-                        })
-            }
-            override fun onDestroy() {
-                super.onDestroy()
-                session.disConnect()
-            }
+        // connection
+        btn_connect.setOnClickListener {
+            session.connect()
         }
+
+        // Disconnect
+        btn_disconnect.setOnClickListener {
+            session.disConnect()
+
+        }
+
+        //unlock
+        btn_unlock.setOnClickListener {
+            session.call(CommandManager.blCommand.unlock(0, System.currentTimeMillis() / 1000, 0))
+                .timeout(3000)
+                .enqueue(object : SessionCallback<Boolean> {
+                    override fun onSuccess(
+                        call: ISessionCall<Boolean>, data:
+                        IResp<Boolean>
+                    ) {
+                        val isSuccess = data.getResult() ?: false
+                        Toast.makeText(
+                            this@BikeTestActivity, if (isSuccess)
+                                "Successfully unlocked" else "Failed to unlock", Toast.LENGTH_SHORT
+                        )
+                            .show()
+                        // Unlock reply
+                        session.call(CommandManager.blCommand.unlockReply()).execute()
+
+                    }
+
+                    override fun onFailure(call: ISessionCall<Boolean>, e: Throwable) {
+
+                        Toast.makeText(
+                            this@BikeTestActivity, "", Toast.LENGTH_SHORT
+                        )
+
+                    }
+                })
+
+        }
+        //Get lock information
+        btn_info.setOnClickListener {
+            session.call(CommandManager.blCommand.getLockInfo())
+                .enqueue(object : SessionCallback<BLInfoResult> {
+                    override fun onSuccess(
+                        call: ISessionCall<BLInfoResult>,
+                        data: IResp<BLInfoResult>
+                    ) {
+                        val result = data.getResult()
+                        if (result != null) {
+                            Log.d("=====", result.toString())
+                        }
+                    }
+
+                    override fun onFailure(
+                        call: ISessionCall<BLInfoResult>, e:
+                        Throwable
+                    ) {
+
+                    }
+                })
+        }
+
+        //Shutdown
+        btn_shutdown.setOnClickListener {
+            session.call(CommandManager.blCommand.shutdown())
+                .enqueue(object : SessionCallback<BLShutdownResult> {
+                    override fun onSuccess(
+                        call: ISessionCall<BLShutdownResult>,
+                        data: IResp<BLShutdownResult>
+                    ) {
+                        val result = data.getResult()
+                        if (result != null) {
+                            Log.d("=====", result.toString())
+                        }
+                    }
+                    override fun onFailure(
+                        call: ISessionCall<BLShutdownResult>,
+                        e: Throwable
+                    ) {
+
+                    }
+                })
+        }
+
+        //Get log
+        btn_get_log.setOnClickListener {
+            session.call(CommandManager.blCommand.getLog())
+                .asyncTimeout(10000)
+                .asyncCall(object : AsyncCallback<String> {
+                    override fun onTimeout() {
+
+                    }
+                    override fun onStarted(success: Boolean) {
+                    }
+
+                    override fun onReceiving(call: ISessionCall<String>, data: IResp<String>) {
+
+                        Log.d("=====", "${data.getResult()}")
+                    }
+                    override fun onReceived() {
+
+                    }
+                })
+        }
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        session.disConnect()
     }
 }
